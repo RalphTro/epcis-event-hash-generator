bbcfb2c28453b725a46198eb27df9dc73796c7e3
Sebastian Schmittner <schmittner@eecc.info>
2020-01-17 21:54:55 +0100

    also test that different representations of the same event have the same hash

--- src/test_required_properties.py
+++ src/test_required_properties.py
@@ -3,8 +3,12 @@ from .EpcisEventHashGenerator import xmlEpcisHash
 from os import walk
 
 TEST_FILE_PATH = "../testFiles/"
+TEST_FILE_PATH_SAME_EVENT = "../testFiles/reordering/"
 
 def testDistinct():
+    """
+    Assert that there are no collisions, i.e. different events must have different hashes.
+    """
     allHashes = []
 
     for (_, _, filenames) in walk(TEST_FILE_PATH):
@@ -12,6 +16,15 @@ def testDistinct():
             if filename.endswith("xml"):
                 allHashes += xmlEpcisHash(TEST_FILE_PATH + filename, "sha256")
         break
-    # assert that there are no duplicates
+    
     assert len(allHashes) == len(set(allHashes))
     
+def testEqual():    
+    """
+    Assert that different representations of the same events have the same hash.
+    """
+    for (_, _, filenames) in walk(TEST_FILE_PATH_SAME_EVENT):
+        for filename in filenames:
+            if filename.endswith("xml"):
+                assert len(set(xmlEpcisHash(TEST_FILE_PATH_SAME_EVENT + filename, "sha256"))) == 1
+        break
