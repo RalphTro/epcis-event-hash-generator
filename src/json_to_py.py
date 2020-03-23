a99e33ffef4b3830c13f4a6ecbf903c95ce8f884
Sebastian Schmittner <schmittner@eecc.info>
2020-03-23 21:15:51 +0100

    new Reference files added yielding same hash

--- src/json_to_py.py
+++ src/json_to_py.py
@@ -5,7 +5,7 @@ See the description of xml_to_py for more details about what 'simple' means here
 For example
 {
   "isA": "ObjectEvent",
-  "eventTime": "2019-04-02T15:00:00+01:00",
+  "eventTime": "2019-04-02T15:00:00.000+01:00",
   "eventTimeZoneOffset": "+01:00",
   "epcList": [
     "urn:epc:id:sgtin:4012345.011111.9876",
@@ -17,7 +17,7 @@ For example
 is converted to
 
 ("ObjectEvent","",[
-  ("eventTime","2019-04-02T15:00:00+01:00", []),
+  ("eventTime","2019-04-02T15:00:00.000+01:00", []),
   ("eventTimeZoneOffset","+01:00", []),
   ("epcList", "", [
     ("epc", "urn:epc:id:sgtin:4012345.011111.5432", []),
@@ -48,20 +48,21 @@ file for details.
 import logging
 import json
 
-
 SKIP_KEYS = ["isA", "#text"]
 
-_namespaces = {} # global dictionary gathered during parsing
+_namespaces = {}  # global dictionary gathered during parsing
+
 
 def namespace_replace(key):
     """If the key contains a namespace (followed by ":"), replace it with
     the {naemspace_url} from the _namespaces dict.
     """
-    splitted = key.split(":",1)
-    if len(splitted)>1:
-        return _namespaces[splitted[0]]+splitted[1]
-    
+    splitted = key.split(":", 1)
+    if len(splitted) > 1:
+        return _namespaces[splitted[0]] + splitted[1]
+
     return key
+
 
 def add_missing_xml_substructure(py_obj):
     """
@@ -69,15 +70,16 @@ def add_missing_xml_substructure(py_obj):
     """
     if py_obj[0] == "readPoint":
         assert not py_obj[2]
-        return (py_obj[0], "", [("id", py_obj[1])])
+        return py_obj[0], "", [("id", py_obj[1], [])]
 
     return py_obj
+
 
 def json_to_py(json_obj):
     """ Recursively convert a string/list/dict to a simple python object
     """
     global _namespaces
-    
+
     py_obj = ("", "", [])
 
     if isinstance(json_obj, list):
@@ -86,26 +88,26 @@ def json_to_py(json_obj):
     elif isinstance(json_obj, dict):
         if "isA" in json_obj:
             py_obj = (json_obj["isA"], "", [])
-            
+
         if "#text" in json_obj:
             py_obj = (py_obj[0], json_obj["#text"], py_obj[2])
-        
-            
+
         for (key, val) in [x for x in json_obj.items() if x[0] not in SKIP_KEYS]:
             if key.startswith("@xmlns"):
                 _namespaces[key[7:]] = "{" + val + "}"
                 logging.debug("Namespaces: %s", _namespaces)
-                
+
                 py_obj = (namespace_replace(py_obj[0]), py_obj[1], py_obj[2])
             else:
                 # first find namespaces in child, then replace in key!
                 child = json_to_py(val)
 
-                key = namespace_replace(key) 
+                key = namespace_replace(key)
 
-                # Names of list elements (e.g. 'epc' elements of 'epsList') are omitted in current json ld form -> restore
+                # Names of list elements (e.g. 'epc' elements of 'epsList') are omitted in current json ld form ->
+                # restore
                 if key.endswith("List"):
-                    named_elements=[]
+                    named_elements = []
                     for element in child[2]:
                         if not element[0]:
                             named_elements.append((key[:-4], element[1], element[2]))
@@ -116,15 +118,15 @@ def json_to_py(json_obj):
 
                 elif isinstance(val, list):
                     for element in child[2]:
-                        py_obj[2].append((key,element[1],element[2]))
+                        py_obj[2].append((key, element[1], element[2]))
                 else:
                     child = add_missing_xml_substructure((key, child[1], child[2]))
-                    py_obj[2].append(child)                                
-                    
+                    py_obj[2].append(child)
+
     else:
         logging.debug("converting '%s' to str", json_obj)
-        return ("",str(json_obj),[])
-    
+        return "", str(json_obj), []
+
     py_obj[2].sort()
     return add_missing_xml_substructure(py_obj)
 
@@ -139,10 +141,9 @@ def event_list_from_epcis_document_json(path):
     json_obj = json.loads(data)
 
     event_list = json_obj["epcisBody"]["eventList"]
-    events=[]
+    events = []
 
     for event in event_list:
         events.append(json_to_py(event))
-    
-    return ("eventList", "", events)
 
+    return ("eventList", "", events)
