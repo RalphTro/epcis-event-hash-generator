d790db8d4caa90061be55b155659552da2e0c2b5
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-12-07 13:43:29 +0100

    added test for bare string normalisation

---
+++ tests/test_bare_string_normalisation.py
@@ -0,0 +1,39 @@
+try:
+    from .context import epcis_event_hash_generator
+except ImportError:
+    from context import epcis_event_hash_generator  # noqa: F401
+
+import xml.etree.ElementTree as ElementTree
+
+from epcis_event_hash_generator.events_from_file_reader import event_list_from_file
+
+
+def test_epcsi_reference_example():
+    actual_obj = event_list_from_file("examples/epcisDocWithSingleEvent.jsonld")
+
+    print(actual_obj)
+
+    expected_obj = ('EventList', '', [
+        ('ObjectEvent', '', [
+            ('action', 'OBSERVE', []),
+            ('bizStep', 'https://ref.gs1.org/cbv/BizStep-shipping', []),
+            ('disposition', 'https://ref.gs1.org/cbv/Disp-in_transit', []),
+            ('eventTime', '2022-02-16T20:33:31.116000-06:00', []),
+            ('eventTimeZoneOffset', '-06:00', []),
+            ('readPoint', '', [
+                ('id', 'urn:epc:id:sgln:0614141.07346.1234', [])
+                ]),
+            ('type', 'ObjectEvent', []),
+            ('bizTransactionList', '', [
+                ('bizTransaction', 'http://transaction.acme.com/po/12345678', [
+                    ('type', 'https://ref.gs1.org/cbv/BTT-po', [])
+                    ])
+                ]),
+            ('epcList', '', [
+                ('epc', 'urn:epc:id:sgtin:0614141.107346.2017', []),
+                ('epc', 'urn:epc:id:sgtin:0614141.107346.2018', [])
+            ])
+        ])])
+
+    assert expected_obj == actual_obj
+    
