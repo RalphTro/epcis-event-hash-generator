41156ea6d05b07faaaa295af6a7601b92073e827
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-12-16 09:55:05 +0100

    added another version of the epcis 2.0 context

--- epcis_event_hash_generator/file_document_loader.py
+++ epcis_event_hash_generator/file_document_loader.py
@@ -25,7 +25,9 @@ def file_document_loader(secure=False, **kwargs):
 
     context_file_hashes = {
         "https://gs1.github.io/EPCIS/epcis-context.jsonld":
-        "14b10c9d3e92d35f577bfc610fe5ec15aa2941124987919389d7cd9998516861"
+        "14b10c9d3e92d35f577bfc610fe5ec15aa2941124987919389d7cd9998516861",
+        "https://ref.gs1.org/standards/epcis/2.0.0/epcis-context.jsonld":
+        "e532647e8eb371379b8b0e8602d8981c8566bc60f7351f22c76a5bc865962008"
     }
 
     def loader(url, options={}):
